a9c64086fe7e19f0b4a4fc551c06009a1d4d42af
Anallely Olivares <tsunllly@Anallelys-MacBook-Pro.local>
2018-10-27 19:11:38 -0500

    Add isPermutation function and its tests

---
+++ src/main/java/CheckPermutation.java
@@ -0,0 +1,76 @@
+public class CheckPermutation {
+
+
+    // Using an integer array as aux
+    static public boolean isPermutation(String a, String b) {
+
+        if (a.length() != b.length()){
+            return false;
+        }
+
+        boolean result = true;
+        int[] aux = new int[256];
+
+        // Initialize aux
+        for( int i = 0; i < aux.length; i++ ) {
+            aux[i] = 0;
+        }
+
+        // Increment in 1
+        for( int i = 0; i < a.length(); i++ ) {
+            int index = (int) a.charAt(i) - 32;
+            aux[index] = aux[index] + 1;
+        }
+
+        // Subtracting in 1
+        for( int i = 0; i < b.length(); i++ ) {
+            int index = (int) b.charAt(i) - 32;
+            aux[index] = aux[index] - 1;
+        }
+
+        // Check aux is all 0's
+        for( int i = 0; i < aux.length; i++ ) {
+            if (aux[i] != 0) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    static public boolean isPermutationN2(String a, String b) {
+        boolean result = true;
+        char restrictedCharacter = '*';
+
+        if (a.length() != b.length()){
+            return false;
+        }
+
+        char[] bb = b.toCharArray();
+
+        // Iterate over a
+        for( int i = 0; i < a.length(); i++ ) {
+
+            // Iterate over b
+            for( int j = 0; j < bb.length; j++ ) {
+                if ( a.charAt(i) == bb[j] ) {
+                    bb[j] = restrictedCharacter;
+                }
+            }
+        }
+
+        // Check bb has all '*'
+        for( int i = 0; i < bb.length; i++ ) {
+            if (bb[i] != restrictedCharacter) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+}
---
+++ src/test/java/CheckPermutationTest.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CheckPermutationTest {
+
+    private String a;
+    private String b;
+
+    private boolean expected;
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][] {
+                        {"arigato", "toiraag", true},
+                        {"abcd", "efcba", false},
+                        {"root", "r0ot", false}
+                }
+        );
+    }
+
+    public CheckPermutationTest(String a, String b, boolean expected) {
+        this.a = a;
+        this.b = b;
+        this.expected = expected;
+    }
+
+    @Test
+    public void isPermutation() {
+        boolean result = new CheckPermutation().isPermutation(a, b);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void isPermutationN2() {
+        boolean result = new CheckPermutation().isPermutationN2(a, b);
+        assertEquals(expected, result);
+    }
+}
